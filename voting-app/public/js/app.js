28b35c1ec2d69d853f1f17c4630fe64bb677aaff
Antonia Bridal <antonia.bridal@rakuten.com>
2019-01-17 17:59:10 +0000

    voting - create ProductList component

---
+++ voting-app/public/js/app.js
@@ -0,0 +1,11 @@
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div className="ui unstackable items">
+                <Product />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<ProductList />, document.getElementById('content'));
